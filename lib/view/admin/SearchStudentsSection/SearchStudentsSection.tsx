c5851bb2c9cc5e830ec88ca2c28c55b32bb02e98
Alejandro Penagos <alejandropenagos1998@gmail.com>
2023-11-25 19:03:04 -0500

    Implement Persons table and search engine for related keywords on Person profile

--- lib/view/admin/SearchStudentsSection/SearchStudentsSection.tsx
+++ lib/view/admin/SearchStudentsSection/SearchStudentsSection.tsx
@@ -1,17 +1,57 @@
-import { useState } from "react";
+import { FormEvent, useEffect, useMemo, useRef, useState } from "react";
 import styles from "./SearchStudentsSection.module.css";
 
 import IPersonDto from "@application/models/IPersonDto";
 import AnimatedPanel from "@view/common/AnimatedPanel";
 import EmploymentInfoDetail from "@view/admin/InfoDetails/EmploymentInfoDetail";
+import useFetchPersons from "../Api/useFetchPersons";
+import Table, { ITableCellIndex, ITableColumn } from "@view/common/Table";
+import ISearchStudentsRow from "./ISearchStudentsRow";
+import { personToRow } from "./SearchStudentsHelpers";
+import Input from "@view/common/Fields/Input";
+import Button from "@view/common/Button";
 
 
 const SearchStudentsSection = () => {
-
+  const [searchString, setSearchString] = useState<string>("");
+  const { persons, loadingPersons } = useFetchPersons({
+    searchString: searchString
+  });
   const [selectedPerson, setSelectedPerson] = useState<undefined | IPersonDto>();
+  const [selectedCell, setSelectedCell] = useState<undefined | ITableCellIndex<ISearchStudentsRow>>();
   const [openLeftPanel, setOpenLeftPanel] = useState(false);
+  const searchInputRef = useRef<HTMLTextAreaElement & HTMLInputElement>(null);
 
-  const onSubmit = () => {};
+  const [columns, setColumns] = useState<ITableColumn<ISearchStudentsRow>[]>([
+    { Header: "#", accessor: "rowIndex", width: 50, cellWidth: 50, resizeDisabled: true },
+    { Header: "Fecha de Registro", accessor: "registerDate", width: 200, cellWidth: 200 },
+    { Header: "Nombres y apellidos", accessor: "fullName", width: 250, cellWidth: 250 },
+  ]);
+
+  const values = useMemo(() => persons
+    ?.sort((a, b) => {
+      const aCreatedAt = new Date(a.createdAt);
+      const bCreatedAt = new Date(b.createdAt);
+      if (aCreatedAt > bCreatedAt) return -1;
+      if (aCreatedAt < bCreatedAt) return 1;
+      return 0;
+    })
+    .map((x, index) => personToRow(x, index)) ?? [],
+    [persons]
+  );
+
+  const onCellClick = (cellIndex: ITableCellIndex<ISearchStudentsRow>) => {
+    setSelectedCell(selectedCell);
+    setSelectedPerson(persons?.find(x => x.id === cellIndex.rowId));
+    setOpenLeftPanel(true);
+  };
+
+  const onSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+    if (searchInputRef.current?.value != null) {
+      setSearchString(searchInputRef.current?.value)
+    }
+  };
 
   return (
     <div className={styles.root}>
@@ -22,21 +62,21 @@ const SearchStudentsSection = () => {
         </div>
       </header>
       <form className={styles.searchBox} onSubmit={onSubmit}>
-         {/*
-            
-          Implemente aquí formulario de búsqueda
-           
-        */}
+        <Input variant="filled" placeholder="Palabras clave" ref={searchInputRef} onChange={(e) => { e.target.value == "" && setSearchString('')}} />
+        <Button variant="contained" color="secondary" type="submit">
+          Buscar
+        </Button>
       </form>
       <main>
-        {/*
-            
-          Implemente aquí la tabla de resultados   
-           
-        */}
-        <div role="dummy-table" className={styles.table}>
-
-        </div>
+        <Table
+          loading={loadingPersons}
+          className={styles.table}
+          columns={columns}
+          values={values}
+          onCellClick={onCellClick}
+          selectedCell={selectedCell}
+          onColumnsChange={setColumns}
+        />
       </main>
       <AnimatedPanel open={openLeftPanel} onClose={() => { setOpenLeftPanel(false); }}>
         {selectedPerson
